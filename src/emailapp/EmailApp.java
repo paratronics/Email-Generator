cab3992e67da9f582d8457288a9a35ac0aaceb9f
Briocodes <briocodes@gmail.com>
2020-10-31 22:33:08 +0100

    email generator features

---
+++ src/emailapp/EmailApp.java
@@ -0,0 +1,10 @@
+package emailapp;
+
+public class EmailApp {
+    public static void main(String[] args) {
+        Email email = new Email("James","Belema");
+
+        email.setAlternateEmail("androidteam@google.com");
+        System.out.println("Your alternate email is: " + email.getAlternateEmail());
+    }
+}
